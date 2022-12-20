513729f3bfdf59c1b4a6fb2202bd2de2381367cb
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-20 17:25:24 +0530

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-/** 1.example--------------------------------------- */
+//3. example --------------------------------------
 
-import "./App.css";
 function App() {
-  let mystyle = { color: "red", background: "green" };
-
   return (
     <div>
-      {/*here we have to change class into class name */}
+      <h1 className="bg-dark text-light p-3">Bootstrap Styling!</h1>
 
-      <h1 className="beautify">dnyana !</h1>
-
-      <h1 style={mystyle}>hi</h1>
-
-      {/* we can write js in {{}} outer is for js internal is for object literal*/}
-
-      <h1 style={{ color: "red", backgroungColor: "purple" }}>
-        Jai Maharashtra
+      <h1 className="bg-success" style={{ color: "#fff" }}>
+        hello
       </h1>
     </div>
   );
 }
+
 export default App;
