a4b22674588de4da336a4a9a12d54312bc19d615
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-20 17:10:38 +0530

    basic  styling raect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-/** 1.example--------------------------------------- */
+/** 2.example--------------------------------------- */
 
-import "./App.css";
+import "./index.js";
+{
+  /**this statement we write in index.js
+because it is globally access file*/
+}
 function App() {
-  let mystyle = { color: "red", background: "green" };
-
   return (
     <div>
-      {/*here we have to change class into class name */}
-
-      <h1 className="beautify">dnyana !</h1>
-
-      <h1 style={mystyle}>hi</h1>
-
-      {/* we can write js in {{}} outer is for js internal is for object literal*/}
-
-      <h1 style={{ color: "red", backgroungColor: "purple" }}>
-        Jai Maharashtra
-      </h1>
+      <h1 className="butify">External CSS styling</h1>
     </div>
   );
 }
