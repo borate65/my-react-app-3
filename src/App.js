f9e7444f100f7c844064245231cb732fddfcd43a
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-20 20:21:54 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,55 +1,35 @@
 import { useState } from "react";
 
 function App() {
-  let [list] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
-  let [theam, setTheam] = useState("primary");
+  let [theam, setTheam] = useState();
 
-  //member Function
-  let makePrimaryTheam = () => {
-    theam = "primary";
-    setTheam(theam);
-  };
-  let makeSuccessTheam = () => {
-    theam = "success";
-    setTheam(theam);
-  };
-  let makeDangerTheam = () => {
-    theam = "danger";
+  let updateTheam = (p1 = "primary") => {
+    //this is member function
+    theam = p1;
     setTheam(theam);
   };
 
   return (
     <div>
-      <div>
-        <h1 className={`bg-${theam} text-light p-3`}>BootStrap Styling!!</h1>
-        <input
-          className="btn btn-primary"
-          type="button"
-          value=""
-          onClick={makePrimaryTheam}
-        />
-        <input
-          className="btn btn-success"
-          type="button"
-          value=""
-          onClick={makeSuccessTheam}
-        />
-        <input
-          className="btn btn-danger"
-          type="button"
-          value=""
-          onClick={makeDangerTheam}
-        />
-      </div>
-      {list.map((item) => (
-        <div className={`alert alert-${theam} m-4`}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia,
-          corrupti commodi assumenda nam ipsam nobis, quis neque nihil magnam
-          deserunt iste. Atque consectetur doloremque tenetur, fugiat accusamus
-          magni aliquid dolorem. lorem
-        </div>
-      ))}
-      ;
+      <h1 className={`bg-${theam} text-light p-3`}>Bootstrap styling theam</h1>
+      <input
+        className="btn btn-primary"
+        type="button"
+        value="primary"
+        onClick={() => updateTheam("primary")}
+      />
+      <input
+        className="btn btn-success"
+        type="button"
+        value="success"
+        onClick={() => updateTheam("success")}
+      />
+      <input
+        className="btn btn-danger"
+        type="button"
+        value="danger"
+        onClick={() => updateTheam("danger")}
+      />
     </div>
   );
 }
