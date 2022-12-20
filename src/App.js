2d7bec5a0678a474dc852fd57ebf703241440104
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-20 17:04:34 +0530

    basic react styling

--- src/App.js
+++ src/App.js
@@ -1,107 +1,23 @@
 /** 1.example--------------------------------------- */
 
-// import "./App.css";
-
-// function App() {
-//   let mystyle = { color: "red", background: "green" };
-
-//   return (
-//     <div>
-//
-
-/* here we have to change class into class name */
-
-//       <h1 className="butify">dnyana !</h1>
-
-//       <h1 style={mystyle}>hi</h1>
-
-//       {/* we can write js in {{}} outer is for js internal is for object literal*/}
-
-//       <h1 style={{ color: "red", backgroungColor: "purple" }}>HEllo World</h1>
-
-//     </div>
-//   );
-// }
-// export default App;
-
-/** 2.example--------------------------------------- */
-
-//import "./index.js"; this statement we write in index.js
-//because it is globally access file
-// function App() {
-//   return (
-//     <div>
-//       <h1 className="butify">External CSS styling</h1>
-//     </div>
-//   );
-// }
-// export default App;
-
-//3. example --------------------------------------
-
-// function App() {
-//   return (
-//     <div>
-//       <h1 className="bg-dark text-light p-3">Bootstrap Styling!</h1>
-
-//       <h1 className="bg-success" style={{ color: "#fff" }}>
-//         hello
-//       </h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//4. example --------------------------------------
-
-import { useState } from "react";
-
+import "./App.css";
 function App() {
-  let [theme, setTheme] = useState("primary");
-
-  // Member funcn
-  let makePrimaryTheme = () => {
-    theme = "primary";
-    setTheme(theme);
-  };
-
-  let makeSuccessTheme = () => {
-    theme = "success";
-    setTheme(theme);
-  };
-
-  let makeDangerTheme = () => {
-    theme = "danger";
-    setTheme(theme);
-  };
+  let mystyle = { color: "red", background: "green" };
 
   return (
     <div>
-      <h1 className={`bg-${theme} text-light p-3`}>
-        Bootstrap Styling! {theme}
-      </h1>
+      {/*here we have to change class into class name */}
 
-      <input
-        className="btn btn-primary"
-        type="button"
-        value="Primary Theme"
-        onClick={makePrimaryTheme}
-      />
-      <input
-        className="btn btn-success"
-        type="button"
-        value="Success Theme"
-        onClick={makeSuccessTheme}
-      />
-      <input
-        className="btn btn-danger"
-        type="button"
-        value="Danger Theme"
-        onClick={makeDangerTheme}
-      />
+      <h1 className="beautify">dnyana !</h1>
+
+      <h1 style={mystyle}>hi</h1>
+
+      {/* we can write js in {{}} outer is for js internal is for object literal*/}
+
+      <h1 style={{ color: "red", backgroungColor: "purple" }}>
+        Jai Maharashtra
+      </h1>
     </div>
   );
 }
-
 export default App;
