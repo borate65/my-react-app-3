c2446ff9464ecb15ae07f9fa96ad6b0519a8f6c3
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-21 20:24:01 +0530

    react

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import axios from "axios";
 import { useState } from "react";
 
 function App() {
-  let [title] = useState("API DEMO");
+  //this is for statefull application
+  let [title] = useState("Api Demo");
   let [messageList, setMessageList] = useState([]);
 
-  let getAllMessages = async () => {
-    //Axios is a promise-based HTTP Client for node.
-    //js and the browser. It is isomorphic (= it can run in the browser and nodejs with the same codebase). On the server-side it uses the native node.js http module, while on the client (browser) it uses XMLHttpRequests.
-    let url = `http://localhost:3001/messages`;
+  let getAllMessage = async () => {
+    let url = `http://localhost:3001/message`;
     let response = await axios.get(url);
-    //console.log(response);
 
-    //getting the message list from server :: and rerendering
+    // console.log(response);
+    // getting the message from server:: And re-rendering
+
     messageList = [...response.data];
     setMessageList(messageList);
+
+    // console.log(response);
+    // Getting the Message From Server :: And re-rendering
+  };
+
+  let createNewMessage = async () => {
+    let url = `http://localhost:3001`;
+    let data = {
+      message: "test Message",
+      messageTime: new Date(),
+      reply: true,
+    };
+    await axios.post(url, data);
   };
 
   return (
@@ -22,8 +35,13 @@ function App() {
       <h1>{title}</h1>
       <input
         type="button"
-        value="make ajax call/api call"
-        onClick={getAllMessages}
+        value="make ajax Api/Get Call"
+        onClick={getAllMessage}
+      />
+      <input
+        type="button"
+        value="make ajax Api/Post Call"
+        onClick={createNewMessage}
       />
       {messageList.map((item) => (
         <div>{item.message}</div>
