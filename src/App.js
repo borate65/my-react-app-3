6670b979685e00c862f576b5d89cb19ae29f6d23
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-21 20:28:24 +0530

    Api demo

--- src/App.js
+++ src/App.js
@@ -2,35 +2,48 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  //data member
+  // Data Member
   let [title] = useState("API DEMO");
+  let [message, setMessage] = useState("");
   let [messageList, setMessageList] = useState([]);
 
-  //special function; Hook:: Like Construtor:: called while the componunt initilized.
-  //like default construtor
+  // Spl Funcn :: Hook :: Like Constructor ::  Called while the Compoent is Initialized.
   useEffect(() => {
-    // console.log("i am getting called");
+    // console.log("I AM GETTING CALLED");
     getAllMessages();
   }, []);
 
-  //member Function
+  // Member Funcns
+  let handleOnChangeMessage = (e) => {
+    message = e.target.value;
+    setMessage(message);
+    // setMessage(e.target.value)
+  };
+
   let getAllMessages = async () => {
     let url = `http://localhost:3001/messages`;
     let response = await axios.get(url);
-    //console.log(response)
+    // console.log(response);
 
+    // Getting the Message From Server :: And re-rendering
     messageList = [...response.data];
     setMessageList(messageList);
   };
+
   let createNewMessage = async () => {
-    let url = `http://localhost:3001`;
+    let url = `http://localhost:3001/message`;
+
     let data = {
-      message: "cdac message",
+      message: message,
       messageTime: new Date(),
       reply: true,
     };
+
     await axios.post(url, data);
-    //to refresh the content
+
+    setMessage("");
+
+    // To Refresh the content
     getAllMessages();
   };
 
@@ -39,21 +52,22 @@ function App() {
       <h1>{title}</h1>
 
       <input
-        type="button"
-        value="Make Ajax/API GET Call"
-        onClick={getAllMessages}
+        type="text"
+        placeholder="Hi...whatsapp...!!"
+        value={message}
+        onChange={handleOnChangeMessage}
       />
-
       <input
         type="button"
         value="Make Ajax/API POST Call"
         onClick={createNewMessage}
       />
 
-      {messageList.map((item) => (
-        <div>{item.message}</div>
+      {messageList.map((item, index) => (
+        <div key={index}>{item.message}</div>
       ))}
     </div>
   );
 }
+
 export default App;
