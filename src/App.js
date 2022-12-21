623b391e4b700345d21b3b484aad65f1278c640d
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-21 20:22:11 +0530

    basic react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import axios from "axios";
 import { useState } from "react";
+
 function App() {
-  let list = ["mumbai", "delhi", "pune", "satara"];
+  let [title] = useState("API DEMO");
+  let [messageList, setMessageList] = useState([]);
+
+  let getAllMessages = async () => {
+    //Axios is a promise-based HTTP Client for node.
+    //js and the browser. It is isomorphic (= it can run in the browser and nodejs with the same codebase). On the server-side it uses the native node.js http module, while on the client (browser) it uses XMLHttpRequests.
+    let url = `http://localhost:3001/messages`;
+    let response = await axios.get(url);
+    //console.log(response);
+
+    //getting the message list from server :: and rerendering
+    messageList = [...response.data];
+    setMessageList(messageList);
+  };
+
   return (
     <div>
-      <h1>Map Demo</h1>
-      {list.map((item) => (
-        <div className="alert alert-primary my-3">{item}</div>
+      <h1>{title}</h1>
+      <input
+        type="button"
+        value="make ajax call/api call"
+        onClick={getAllMessages}
+      />
+      {messageList.map((item) => (
+        <div>{item.message}</div>
       ))}
     </div>
   );
