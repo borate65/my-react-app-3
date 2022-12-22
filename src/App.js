74e0392952c0e2f486f83e4430acb8117e8dd7d3
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-22 13:46:18 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import AppBody from "./componunts/AppBody";
-import AppFooter from "./componunts/AppFooter";
-import AppHeader from "./componunts/AppHeader";
+//8 import { Route, Routes } from "react";
+
+// function App() {
+//   return (
+//     <Routes>
+//       {/** http://localhost:3000/ */}
+//       <Route path="/" element={<h1>Home</h1>} />
+
+//       {/** http://localhost:3000/home */}
+//       <Route path="/home" element={<h1>Home</h1>} />
+
+//       {/** http://localhost:3000/explore */}
+//       <Route path="/explore" element={<h1>Explore</h1>} />
+
+//       {/** http://localhost:3000/notifications */}
+//       <Route path="/notifications" element={<h1>Notifications</h1>} />
+
+//       <Route path="*" element={<h1>Page Not Found</h1>} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
+//9
+//route is used for multopage applictaion
+
+import { Link, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div>
-      <AppHeader />
-      <AppBody />
-      <AppFooter />
+      <Link to="/home" className="fs-4">
+        Home |
+      </Link>
+      <Link to="/explore" className="fs-4">
+        Explore |
+      </Link>
+      <Link to="/notifications" className="fs-4">
+        Notifications
+      </Link>
+
+      <Routes>
+        {/** http://localhost:3000/ */}
+        <Route path="/" element={<h1>Home</h1>} />
+
+        {/** http://localhost:3000/home */}
+        <Route path="/home" element={<h1>Home</h1>} />
+
+        {/** http://localhost:3000/explore */}
+        <Route path="/explore" element={<h1>Explore</h1>} />
+
+        {/** http://localhost:3000/notifications */}
+        <Route path="/notifications" element={<h1>Notifications</h1>} />
+
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
     </div>
   );
 }
+
 export default App;
