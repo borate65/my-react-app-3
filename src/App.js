5dc71a39be598a10ac5226eb68bcb9bff25aa40a
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-25 02:28:59 +0530

    basic redux

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import { useState } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-    <ErrorBoundary FallbackComponent={HandleError}>
-      <HelloWorld />
-    </ErrorBoundary>
+    <div>
+      <Counter1 />
+      <hr />
+      <Counter2 />
+    </div>
   );
-
-  function HandleError({ error }) {
-    console.log("This error occured", error);
-    return (
-      <div>
-        <h1>An Error occured</h1>
-      </div>
-    );
-  }
-
-  function HelloWorld() {
-    let [user] = useState({ id: 1, name: "dnyaneshawar" });
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <h1>{user}</h1>
-      </div>
-    );
-  }
 }
+
+function Counter1() {
+  let [amount, setAmount] = useState(100);
+
+  let deposit = () => {
+    amount += 50;
+    setAmount(amount);
+  };
+
+  return (
+    <div>
+      <h1>Counter 1 - Amount {amount}</h1>
+      <input type="button" value="Deposit" onClick={deposit} />
+    </div>
+  );
+}
+
+function Counter2() {
+  let [amount, setAmount] = useState(100);
+
+  let deposit = () => {
+    amount += 50;
+    setAmount(amount);
+  };
+
+  return (
+    <div>
+      <h1>Counter 2 - Amount {amount}</h1>
+      <input type="button" value="Deposit" onClick={deposit} />
+    </div>
+  );
+}
+
 export default App;
