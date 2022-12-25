01476e9fbea16eab5966fe482e4bb8e77cd1afa5
Dnyaneshwar <boratednyaneshwar65@gmail.com>
2022-12-25 20:38:32 +0530

    basic recat-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// GLOBAL
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -8,13 +9,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //add this tag <browserRouter> and also 9 no.line
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
